c535f05ffb6817c02879e7c520eecde9c454a1cf
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-05-15 00:28:39 -0300

    Entities created Book and Author

---
+++ src/main/java/org/lab/testing/bookstoremanager/entity/Author.java
@@ -0,0 +1,34 @@
+package org.lab.testing.bookstoremanager.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	@Getter @Setter private String name;
+	
+	@Column(nullable = false)
+	private Integer age;
+	
+}
